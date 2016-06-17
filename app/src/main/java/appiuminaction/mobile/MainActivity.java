65427c9f5e1c7415d93e33f27fa1bf1a2fd25261
Vijay Patel <vijay@spiritedtechie.com>
2016-06-17 11:16:31 +0100

    Created project structure and a test Android app ready for appium testing.

---
+++ app/src/main/java/appiuminaction/mobile/MainActivity.java
@@ -0,0 +1,38 @@
+package appiuminaction.mobile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final EditText et_name = (EditText) findViewById(R.id.et_name);
+        Button btn_submit = (Button) findViewById(R.id.btn_submit);
+
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent acceptedIntent = new Intent(MainActivity.this, AcceptedActivity.class);
+                acceptedIntent.putExtra("NAME", et_name.getText().toString());
+                startActivity(acceptedIntent);
+                finish();
+            }
+        });
+    }
+}
